fc01963fd2302efa95e567279a3456892643bcf5
roman10rb <roma10rb@gmail.com>
2023-03-07 19:33:19 +0200

    part 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const createGalleryElements = createGallery(galleryItems);
+
+galleryContainer.innerHTML = createGalleryElements;
+
+function createGallery(gallery) {
+  return gallery.map(({preview, original, description}) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>
+    `
+  }).join('');
+
+};
+galleryContainer.addEventListener('click', handleImg);
+
+function handleImg(event) {
+    event.preventDefault();
+     if ( event.target.nodeName !== 'IMG') {
+    return;
+     }
+};
+
+
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionDelay: 250, 
+ });
